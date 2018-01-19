a0cf2571f1378b08540d2e88c4126a3942f6502c
ROHIT <rpshooter02@yahoo.in>
2018-01-20 01:35:25 +0530

    click function version 2

--- app/src/main/java/com/softgallary/rohit/Test/MainActivity.java
+++ app/src/main/java/com/softgallary/rohit/Test/MainActivity.java
@@ -2,6 +2,9 @@ package com.softgallary.rohit.Test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //on click text
+        TextView hello =findViewById(R.id.txt_hello);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"This is Version 2",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
